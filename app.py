57c847f01f1bddab76a769a077bb28c10f8adbb5
Abhimanyu Singh <55909380+manyu314@users.noreply.github.com>
2021-06-28 03:34:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pickle
+import string
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+
+app = Flask(__name__)
+
+model = pickle.load(open('sms.pkl', 'rb'))
+
+
+@app.route('/')
+def main():
+    return render_template('first.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    sms = request.form['sms']
+    if sms == '':
+        return render_template(blank.html)
+
+    # Remove Punctuation
+    punct = string.punctuation + '\n'
+
+    def remove_punct(text):
+        new_text = ''
+        for char in text:
+            if char in string.punctuation:
+                continue
+            else:
+                new_text += char
+        return new_text
+
+    sms = remove_punct(sms)
+
+    # Stemming
+    p_stemmer = PorterStemmer()
+
+    # Function to do the stemming
+    def stemming(text):
+        text = text.split(' ')  # to seperate the words
+        text = [p_stemmer.stem(word) for word in text]
+
+        return text
+
+    sms = stemming(sms)
+
+    result = model.predict(sms)
+
+    return render_template('prediction.html', result=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
